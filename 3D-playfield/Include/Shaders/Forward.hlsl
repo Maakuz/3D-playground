6c1df4d7916f17d914ea98f09ec8b5983cc0b0bb
Markus FAST HEMMA <markuswerdien@hotmail.com>
2017-09-28 06:53:44 +0200

    Immobile normals, not sure if this is good, I think it is

--- 3D-playfield/Include/Shaders/Forward.hlsl
+++ 3D-playfield/Include/Shaders/Forward.hlsl
@@ -11,9 +11,11 @@ struct VS_IN
 struct GS_IN
 {
     float4 pos : POSITION;
-    float4 wPos : WORLDPOSITION;
+    //float4 wPos : WORLDPOSITION;
     float3 normal : NORMAL;
     float2 uv : UV;
+
+    float4x4 transform : TRANSFORM;
 };
 
 struct PS_IN
@@ -34,61 +36,62 @@ cbuffer camera : register(b0)
 
 cbuffer flashLight : register(b1)
 {
-    float4 lightPos;
-    float4 lightDir;
-    bool lightOn;
+    float4 flashLightPos;
+    float4 flashLightDir;
+    int flashLightOn;
 };
 
 GS_IN VS(VS_IN input)
 {
     GS_IN output = (GS_IN) 0;
 
-    output.wPos = float4(input.pos, 1);
+    output.pos = float4(input.pos, 1);
 
-    output.wPos = mul(input.transform, output.wPos);
-    output.pos = mul(VP, output.wPos);
+    //output.pos = mul(input.transform, output.wPos);
+    //output.pos = mul(VP, output.pos);
 
-    output.normal = normalize(mul(input.transform, float4(input.normal, 0)));
+    output.normal = input.normal;// normalize(mul(input.transform, float4(input.normal, 0)));
     output.uv = input.uv;
 
+    output.transform = input.transform;
 
     return output;
 }
 
-//Credits to Ipren
+//Credits to Ipren CAN YOU DO THIS BEFORE STARTING AND STORE THEM????
 [maxvertexcount(3)]
 void GS(triangle GS_IN input[3] : SV_POSITION, inout TriangleStream<PS_IN> output)
 {
      //calculats tangent and bitangent for normal mapping
-    float3 e1 = input[1].wPos - input[0].wPos;
-    float3 e2 = input[2].wPos - input[0].wPos;
-    float2 dUV1 = input[1].uv - input[0].uv;
-    float2 dUV2 = input[2].uv - input[0].uv;
+    float3 e0 = input[1].pos - input[0].pos;
+    float3 e1 = input[2].pos - input[0].pos;
+    float2 dUV0 = input[1].uv - input[0].uv;
+    float2 dUV1 = input[2].uv - input[0].uv;
 
         // 1 diveded by determinant constant
-    float c = 1.0f / (dUV1.x * dUV2.y - dUV2.x * dUV1.y);
+    float c = 1.0f / (dUV0.x * dUV1.y - dUV1.x * dUV0.y);
 
         //tangent
     float3 t;
-    t.x = c * (dUV2.y * e1.x - dUV1.y * e2.x);
-    t.y = c * (dUV2.y * e1.y - dUV1.y * e2.y);
-    t.z = c * (dUV2.y * e1.z - dUV1.y * e2.z);
+    t.x = c * (dUV1.y * e0.x - dUV0.y * e1.x);
+    t.y = c * (dUV1.y * e0.y - dUV0.y * e1.y);
+    t.z = c * (dUV1.y * e0.z - dUV0.y * e1.z);
     t = normalize(t);
 
         //bitangent
     float3 bt;
-    bt.x = c * (-dUV2.x * e1.x + dUV1.x * e2.x);
-    bt.y = c * (-dUV2.x * e1.y + dUV1.x * e2.y);
-    bt.z = c * (-dUV2.x * e1.z + dUV1.x * e2.z);
+    bt.x = c * (-dUV1.x * e0.x + dUV0.x * e1.x);
+    bt.y = c * (-dUV1.x * e0.y + dUV0.x * e1.y);
+    bt.z = c * (-dUV1.x * e0.z + dUV0.x * e1.z);
     bt = normalize(bt);
 
 
     for (uint i = 0; i < 3; i++)
     {
         PS_IN element;
-        element.pos = input[i].pos;
-        element.wPos = input[i].wPos;
-        element.normal = input[i].normal;
+        element.wPos = mul(input[i].transform, input[i].pos);
+        element.pos = mul(VP, element.wPos);
+        element.normal = mul(input[i].transform, input[i].normal);
         element.uv = input[i].uv;
         element.tangent = t;
         element.biTangent = bt;
@@ -101,27 +104,55 @@ SamplerState sState : register(s0);
 texture2D diffuseTexture : register(t0);
 texture2D normalTexture : register(t1);
 
-float4 PS(PS_IN input) : SV_Target0
+float3 sampleNormal(PS_IN input)
 {
-    float3 lightDir = normalize(float3(0.3, -1, 0.3));
-    float3 lightColor = 1;
-
-    ////NORMAL MAPPING
     float3 normalSample = normalTexture.Sample(sState, input.uv);
-
-    float3x3 tangentMatrix = float3x3(input.tangent, input.biTangent, input.normal);
+    
     normalSample = normalize(normalSample * 2.0 - 1);
 
+    input.tangent = normalize(input.tangent - dot(input.tangent, input.normal) * input.normal);
 
-    float3 finalNormal = normalize(mul(normalSample, tangentMatrix));
-    ///////////
+    float3x3 tangentMatrix = float3x3(input.tangent, input.biTangent, input.normal);
+
+
+    return normalize(mul(normalSample, tangentMatrix));
+}
+
+float4 PS(PS_IN input) : SV_Target0
+{
+    float3 finalNormal = sampleNormal(input);
+    
+    float3 lightDir = normalize(float3(0, -0.5, 0.5));
+    float3 lightColor = 1;
+    
+    float3 diffuseComponent = saturate(dot(finalNormal, -lightDir)) * lightColor;
+
+
+    ///FlashLight
+    float outerCone = 0.8f;
+    float innerCone = 0.95f;
+    float innerMinusOuter = innerCone - outerCone;
+    float3 flashLightToPos = input.wPos.xyz - flashLightPos.xyz;
+
+
+    float cosAngle = dot(normalize(flashLightToPos), normalize(flashLightDir.xyz));
+
+    float falloff = flashLightOn ? saturate((cosAngle - outerCone) / innerMinusOuter) : 0;
+
+    //Test both and see difference
+    //float attenuation = saturate(1.f / (0.01 * max(20, pow(length(flashLightToPos.xyz), 2))));
+    float attenuation = smoothstep(40, 0, length(flashLightToPos));
+
+    float3 lightDiffuseComponent = dot(finalNormal, normalize(-flashLightToPos)) * attenuation * falloff;
+    lightDiffuseComponent = saturate(lightDiffuseComponent);
 
 
 
-    float3 diffuseComponent = dot(finalNormal, -lightDir) * lightColor;
 
     float3 color = diffuseTexture.Sample(sState, input.uv);
 
+    float3 totalIllumination = diffuseComponent + lightDiffuseComponent;
 
-    return float4(color * diffuseComponent, 1);
+    return float4(diffuseComponent, 1);
+    return float4(totalIllumination * color, 1);
 }
